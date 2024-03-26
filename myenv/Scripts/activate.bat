6ccf18cfe5a233b59f37e4bb62233307dc549bae
Cibele Mallmann <cibelermallmann@gmail.com>
2024-03-26 18:52:04 -0300

    pytest

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cibele.mallmann\Downloads\selenium_python\myenv
+set VIRTUAL_ENV=C:\python_automation\myenvC:\python_automation\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
